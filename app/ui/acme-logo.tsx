671399bb5e6569d86a0929b1cd7e8feebd09d040
Nguyen Khanh Hoa <hoank@truerace.org>
2024-09-13 16:03:11 +0700

    done

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,7 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+// import { lusitana } from '@/app/ui/fonts';
+import { Inter, Lusitana } from 'next/font/google';
 
 export default function AcmeLogo() {
+  const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
